4eda6b50ce121128c260ec0aeb4a2a1a73a639c9
Lnc <hnc@singularity.fr>
2013-05-08 22:48:20 +0200

    Add template function which returns Pi

---
+++ include/hnc/math/pi.hpp
@@ -0,0 +1,35 @@
+// Copyright © 2012 Lénaïc Bagnères, hnc@singularity.fr
+
+// This file is part of hnc.
+
+// hnc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hnc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with hnc. If not, see <http://www.gnu.org/licenses/>
+
+
+#ifndef HNC_MATH_PI_HPP
+#define HNC_MATH_PI_HPP
+
+#include <cmath>
+
+
+namespace hnc
+{
+	namespace math
+	{
+		/// Pi constante (3.141592653589793)
+		template <class T = double>
+		T constexpr pi() { return double(4.) * std::atan(double(1.)); }
+	}
+}
+
+#endif
---
+++ tests/test__math_pi.cpp
@@ -0,0 +1,42 @@
+// Copyright © 2012 Lénaïc Bagnères, hnc@singularity.fr
+
+// This file is part of hnc.
+
+// hnc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hnc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with hnc. If not, see <http://www.gnu.org/licenses/>
+
+
+#include <iostream>
+
+#include <hnc/math/pi.hpp>
+#include <hnc/test.hpp>
+#include <hnc/to_string.hpp>
+
+
+int main()
+{
+	int nb_test = 0;
+
+	++nb_test;
+	{
+		std::string const ref_pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+		std::string const pi = hnc::to_string(hnc::math::pi());
+		std::cout << "hnc::math::pi = " << pi << std::endl;
+		std::cout << "Reference pi  = " << ref_pi << std::endl;
+		nb_test -= hnc::test::warning(pi == "3.141592653589793", "hnc::math::pi == " + pi + " instead of 3.141592653589793\n");
+	}
+
+	hnc::test::warning(nb_test == 0, "hnc::math::pi: " + hnc::to_string(nb_test) + " test fail!\n");
+
+	return nb_test;
+}
