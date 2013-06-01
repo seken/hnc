d36832caafa8991b984de2ace13e2ea64a2f9239
Lnc <hnc@singularity.fr>
2013-06-01 23:39:46 +0200

    Add hnc::except::incomplete_implementation, a exception when you identify a particular case and you do not have not time to develop it

---
+++ include/hnc/except.hpp
@@ -0,0 +1,81 @@
+// Copyright © 2013 Lénaïc Bagnères, hnc@singularity.fr
+
+// This file is part of hnc.
+
+// hnc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hnc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with hnc. If not, see <http://www.gnu.org/licenses/>
+
+
+#ifndef HNC_EXCEPT_HPP
+#define HNC_EXCEPT_HPP
+
+#include <exception>
+#include <stdexcept>
+
+
+namespace hnc
+{
+	/**
+	 * @brief Extends std::exception, std::logic_error and std::runtime_error
+	 *
+	 * @code
+	 * #include <hnc/except.hpp>
+	 * @endcode
+	 *
+	 * std::exception @n
+	 * http://www.cplusplus.com/reference/exception/exception/ @n
+	 * http://en.cppreference.com/w/cpp/error/exception
+	 *
+	 * std::logic_error @n
+	 * http://www.cplusplus.com/reference/stdexcept/logic_error/ @n
+	 * http://en.cppreference.com/w/cpp/error/logic_error
+	 *
+	 * std::runtime_error @n
+	 * http://www.cplusplus.com/reference/stdexcept/runtime_error/ @n
+	 * http://en.cppreference.com/w/cpp/error/runtime_error
+	 */
+	namespace except
+	{
+		/// Exception to report incomplete implementation
+		class incomplete_implementation : public std::logic_error
+		{
+		public:
+
+			/**
+			 * @brief Constructor
+			 *
+			 * @param[in] what_arg Description of the error
+			 */
+			explicit incomplete_implementation (std::string const & what_arg) :
+				std::logic_error(what_arg)
+			{ }
+
+			/// @copydoc hnc::except::incomplete_implementation::incomplete_implementation(std::string const &)
+			explicit incomplete_implementation (char const * const what_arg) :
+				std::logic_error(what_arg)
+			{ }
+
+			/// Return the description of the error
+			/// @return the description of the error
+			virtual char const * what() const noexcept
+			{
+				return std::logic_error::what();
+			}
+
+			/// Destructor
+			virtual ~incomplete_implementation() noexcept { }
+		};
+	}
+}
+
+#endif
---
+++ tests/test__except.cpp
@@ -0,0 +1,54 @@
+// Copyright © 2012 Inria, Written by Lénaïc Bagnères, lenaic.bagneres@inria.fr
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <iostream>
+
+#include <hnc/except.hpp>
+#include <hnc/test.hpp>
+#include <hnc/to_string.hpp>
+
+
+int main()
+{
+	int nb_test = 0;
+
+	++nb_test;
+	try
+	{
+		throw hnc::except::incomplete_implementation(std::string("hnc::except::incomplete_implementation with std::string"));
+	}
+	catch (hnc::except::incomplete_implementation const & e)
+	{
+		std::cerr << e.what() << std::endl;
+		--nb_test;
+	}
+	std::cout << std::endl;
+
+	++nb_test;
+	try
+	{
+		throw hnc::except::incomplete_implementation("hnc::except::incomplete_implementation with char const * const");
+	}
+	catch (hnc::except::incomplete_implementation const & e)
+	{
+		std::cerr << e.what() << std::endl;
+		--nb_test;
+	}
+	std::cout << std::endl;
+	
+	hnc::test::warning(nb_test == 0, "hnc::except " + hnc::to_string(nb_test) + " test fail!\n");
+
+	return nb_test;
+}
