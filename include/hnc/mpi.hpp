5a0eade6d8d8599b0847e08889ce42c4031c0d4c
Lnc <hnc@singularity.fr>
2013-06-18 03:17:25 +0200

    Upgrade hnc::mpi namespace doxygen

--- include/hnc/mpi.hpp
+++ include/hnc/mpi.hpp
@@ -32,12 +32,67 @@ namespace hnc
 	 * #include <hnc/mpi.hpp>
 	 * @endcode
 	 *
-	 * @b From @b Wikipedia http://en.wikipedia.org/wiki/Message_Passing_Interface @n
-	 * Message Passing Interface (MPI) is a standardized and portable message-passing system designed by a group of researchers from academia and industry to function on a wide variety of parallel computers. The standard defines the syntax and semantics of a core of library routines useful to a wide range of users writing portable message-passing programs in Fortran 77 or the C programming language. Several well-tested and efficient implementations of MPI include some that are free and in the public domain. These fostered the development of a parallel software industry, and there encouraged development of portable and scalable large-scale parallel applications.
-	 *
 	 * With MPI you can write distributed program to:
 	 * - increase performances
 	 * - solve a bigger problem who can do with only one computer
+	 *
+	 * @b From @b Wikipedia http://en.wikipedia.org/wiki/Message_Passing_Interface @n
+	 * Message Passing Interface (MPI) is a standardized and portable message-passing system designed by a group of researchers from academia and industry to function on a wide variety of parallel computers. The standard defines the syntax and semantics of a core of library routines useful to a wide range of users writing portable message-passing programs in Fortran 77 or the C programming language. Several well-tested and efficient implementations of MPI include some that are free and in the public domain. These fostered the development of a parallel software industry, and there encouraged development of portable and scalable large-scale parallel applications.
+	 *
+	 * To use MPI code, you need Boost.MPI and Boost.Serialization @n
+	 * You can add this in your CMakeLists.txt
+	 * @code
+	 * find_package(Boost COMPONENTS mpi serialization)
+	 * if (Boost_FOUND)
+	 * 	# Boost.MPI and Boost.Serialization
+	 * 	set(boost_mpi_serialization "${Boost_LIBRARIES}")
+	 * 	message(STATUS "Boost.MPI and Boost.Serialization found :) ${boost_mpi_serialization}")
+	 * 	# MPI compiler
+	 * 	find_program(MPICXX_COMPILER NAMES mpic++ mpicxx mpiCC mpicc)
+	 * 	if (MPICXX_COMPILER)
+	 * 		# Change the compiler
+	 * 		message(STATUS "MPI C++ compiler found = ${MPICXX_COMPILER}")
+	 * 		set(CMAKE_CXX_COMPILER ${MPICXX_COMPILER})
+	 * 	else()
+	 * 		message(STATUS "MPI C++ compiler not found :(")
+	 * 	endif()
+	 * 	# MPI run
+	 * 	find_program(MPIRUN_EXE NAMES mpirun)
+	 * 	if (MPIRUN_EXE)
+	 * 		message(STATUS "mpirun executable found = ${MPIRUN_EXE}")
+	 * 	else()
+	 * 		message(STATUS "mpirun executable not found :(")
+	 * 	endif()
+	 * else()
+	 * 	message(STATUS "Boost.MPI and Boost.Serialization not found :(")
+	 * endif()
+	 * 
+	 * # ...
+	 *
+	 * # Create the executable
+	 * add_executable(your_exe your_exe.cpp)
+	 * target_link_libraries(your_exe ${boost_mpi_serialization})
+	 * @endcode
+	 * 
+	 * For quick test, if you use GCC from the command line and Boost is installed, you can link directly with:
+	 * @code
+	 * -lboost_mpi -lboost_serialization
+	 * @endcode
+	 *
+	 * On Debian GNU/Linux distribution you can install the packages:
+	 * @code
+	 * openmpi-bin openmpi-dev libboost-serialization-dev libboost-mpi-dev
+	 * @endcode
+	 *
+	 * To run a program with MPI, run your program with:
+	 * @code
+	 * mpirun -np number_of_processors your_exe
+	 * @endcode
+	 * 
+	 * If you want compile without Boost.MPI, you can define the macro
+	 * @code
+	 * #define NO_HNC_Boost_MPI
+	 * @endcode
 	 */
 	namespace mpi
 	{
