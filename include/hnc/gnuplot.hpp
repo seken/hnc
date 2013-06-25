05caafd54f8600c48e2110b1b2ea892e83c56d87
Lnc <hnc@singularity.fr>
2013-06-26 01:56:40 +0200

    Add hnc::gnuplot

---
+++ include/hnc/gnuplot.hpp
@@ -0,0 +1,44 @@
+// Copyright © 2012 Lénaïc Bagnères, hnc@singularity.fr
+
+// This file is part of hnc.
+
+// hnc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hnc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with hnc. If not, see <http://www.gnu.org/licenses/>
+
+
+#ifndef HNC_GNUPLOT_HPP
+#define HNC_GNUPLOT_HPP
+
+#include "gnuplot/gnuplot.hpp"
+#include "gnuplot/gnuplot_boxes.hpp"
+
+
+namespace hnc
+{
+	/**
+	 * @brief To create some Gnuplot script and data
+	 *
+	 * @code
+	 * #include <hnc/gnuplot.hpp>
+	 * @endcode
+	 *
+	 * 2D Gnuplot hnc::gnuplot::gnuplot :
+	 * - Histogram hnc::gnuplot::gnuplot_boxes
+	 */
+	namespace gnuplot
+	{
+		// For Doxygen only
+	}
+}
+
+#endif
