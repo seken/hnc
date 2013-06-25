f1e5ad30f8f96de3be0a0c63f51e4c18004341af
Lnc <hnc@singularity.fr>
2013-06-26 01:58:33 +0200

    Add hnc::gnuplot::style (not implemented)

---
+++ include/hnc/gnuplot/style.hpp
@@ -0,0 +1,67 @@
+// Copyright © 2013 Lénaïc Bagnères, hnc@singularity.fr
+
+// This file is part of hnc.
+
+// hnc is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// hnc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with hnc. If not, see <http://www.gnu.org/licenses/>
+
+
+/**
+ * @file
+ * @brief Just for std::ostream & operator<<(std::ostream & o, hnc::gnuplot::style const & style)
+ */
+
+#ifndef HNC_GNUPLOT_STYLE_HPP
+#define HNC_GNUPLOT_STYLE_HPP
+
+#include <string>
+#include <vector>
+
+
+namespace hnc
+{
+	namespace gnuplot
+	{
+		// TODO
+		
+		/**
+		 * @brief The commands ‘set style data‘ and ‘set style function‘ change the default plotting style for subsequent ‘plot‘ and ‘splot‘ commands
+		 * 
+		 * @code
+		 * #include <hnc/gnuplot.hpp>
+		 * @endcode
+		 *
+		 * http://gnuplot.sourceforge.net/docs_4.2/node231.html
+		 *
+		 * @warning This class is not implemented yet
+		 */
+		class style
+		{
+		};
+
+		/**
+		 * @brief Display a hnc::gnuplot::style
+		 *
+		 * @param[out] o     Out stream
+		 * @param[in]  style A hnc::gnuplot::style
+		 *
+		 * @return the out stream
+		 */
+		std::ostream & operator<<(std::ostream & o, hnc::gnuplot::style const & /*style*/)
+		{
+			return o;
+		}
+	}
+}
+
+#endif
